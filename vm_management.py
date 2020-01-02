23ea5143fddb807f4a2afbe7ffff7eeca58d8b1a
Omri Golan <omrig@anyvision.com>
2020-01-02 12:23:47 +0200

    VM manager - fixed issue with execting commands as list Added gitignore file

--- vm_management.py
+++ vm_management.py
@@ -3,16 +3,14 @@ import subprocess
 
 class VmManager(object):
     def exec_command(self, command):
-        with subprocess.Popen([command], stdout=subprocess.PIPE) as proc:
+        with subprocess.Popen(command.split(), stdout=subprocess.PIPE) as proc:
             return proc.stdout.read()
 
-    def force_stop_vm(self, vm):
+    def list_vms(self, args):
+        return f"virsh list {args}"
+
+    def force_shutdown_vm(self,vm):
         return f"virsh destory {vm}"
 
-    def gracefully_stop_vm(self, vm):
-        return f"virsh shutdown {vm}"
 
-    def list_vms(self):
-        return f"virsh list" \
-               f""
 
