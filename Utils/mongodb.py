895a79de24b783242475163c2e015b295abe2868
Omri Golan <omrig@anyvision.com>
2020-01-02 15:48:53 +0200

    Attempt to support mongo connection

--- Utils/mongodb.py
+++ Utils/mongodb.py
@@ -1,1 +1,15 @@
-#TODO: connect to mongo return data init
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+
+# TODO: connect to mongo return data init
+
+
+class Mongo(object):
+    def connect(self, mongo_user, mongo_password, mongo_host_port_array, mongo_db, mongo_auth_db='admin', rs='rs0'):
+        uri = f"mongodb://{quote_plus(mongo_user)}:{quote_plus(mongo_password)}@{mongo_host_port_array}/{mongo_db}?authSource={mongo_auth_db}"
+        client = MongoClient(uri, w=1, journal=True, replicaSet=rs)
+        if client:
+            return client
+        else:
+            return None
+
