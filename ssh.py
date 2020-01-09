97121adb4f6cfa02cbbe14977fde901e33f2b7cb
Omri Golan <omrig@anyvision.com>
2020-01-09 21:50:32 +0200

    Get password for ssh from config

--- ssh.py
+++ ssh.py
@@ -9,8 +9,10 @@ script_path = "disconnect_site_from_hq.sh"
 def disconnect_site_from_hq(site_config, ssh_config):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
     ssh.connect(hostname=site_config['site_extarnel_ip'], username=ssh_config['username'],
-                key_filename=ssh_config['pem_path'])
+                password=ssh_config['password'])
+                # key_filename=ssh_config['pem_path'])
     sftp = ssh.open_sftp()
     sftp.put(f"{file_path}", f"/tmp/{script_path}", confirm=True)
     stdin, stdout, stderr= ssh.exec_command(f"bash /tmp/{script_path}")
