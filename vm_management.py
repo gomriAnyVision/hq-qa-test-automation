20e97ae401a4647d01ad5174dcb8ab5dd36ba438
Omri Golan <omrig@anyvision.com>
2020-01-09 21:11:00 +0200

    Now able to get the allocator ip from the conig

--- vm_management.py
+++ vm_management.py
@@ -2,12 +2,9 @@ from pprint import pprint
 
 import requests
 
-ALLOCATOR_IP = "192.168.21.87"
-ALLOCATOR_URL = "http://{}:8080/vms".format(ALLOCATOR_IP)
 
-
-def update_vm_status(vm_name, power):
-    request_url = "http://{}:8080/vms/{}/status".format(ALLOCATOR_IP, vm_name)
+def update_vm_status(vm_name, power, config):
+    request_url = "http://{}:8080/vms/{}/status".format(config['allocator_ip'], vm_name)
     res = requests.post(request_url, data={"power": power})
     assert res.status_code == 200
     return res.json()
