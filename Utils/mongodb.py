030c6f414fa7b35be5d9e038d94ff4b069b9d659
Omri Golan <omrig@anyvision.com>
2020-01-09 17:16:18 +0200

    Added class which init mongo client with the following functions - _get_list_sites: get a list of all sites connect to the HQ by querying the sites table in the mapi db and looping though the results and inserting them in to a list to make it easyer to work with and to protect the db from being queryed to many times to get a list of sites - count_subjects: does the name indicates - site_sync_status: gets a db object and search for the sync status of each site in the returned by the _get_list_sites - get_db: returns a db instance from the client

--- Utils/mongodb.py
+++ Utils/mongodb.py
@@ -1,15 +1,34 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
-# TODO: connect to mongo return data init
 
-
-class Mongo(object):
-    def connect(self, mongo_user, mongo_password, mongo_host_port_array, mongo_db, mongo_auth_db='admin', rs='rs0'):
+class MongoDB(object):
+    def __init__(self, mongo_user="root", mongo_password="NjMxNmQ3MTBjNGVjNGZlZDRhYzc0NjU2"
+                , mongo_host_port_array="34.76.123.137"
+                , mongo_db="mapi", mongo_auth_db='admin', rs='rs0'):
         uri = f"mongodb://{quote_plus(mongo_user)}:{quote_plus(mongo_password)}@{mongo_host_port_array}/{mongo_db}?authSource={mongo_auth_db}"
-        client = MongoClient(uri, w=1, journal=True, replicaSet=rs)
-        if client:
-            return client
-        else:
-            return None
+        self.client = MongoClient(uri, w=1, journal=True, replicaSet=rs)
 
+    def _get_list_sites(self, db):
+        self.site_list = []
+        res = db.sites.find({})
+        for site in res:
+            self.site_list.append(site)
+        return self.site_list
+
+    def count_subjects(self, db):
+        return db.subjects.count()
+
+    def site_sync_status(self, db):
+        site_list = self._get_list_sites(db)
+        for site in site_list:
+            return site['syncStatus'], site['_id']
+
+    def get_db(self, db):
+        return self.client[db]
+
+
+if __name__ == '__main__':
+    client = MongoDB()
+    mapi = client.get_db('mapi')
+    client.site_sync_status(mapi)
