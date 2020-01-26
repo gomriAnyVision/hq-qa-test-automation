fc6b2759978a78b876a56e9fa9a86bc62b13278a
Omri Golan <omrig@anyvision.com>
2020-01-26 17:49:45 +0200

    Uncomment mongodb.py that was commented out in order to test the automation_infra project

--- Utils/mongodb.py
+++ Utils/mongodb.py
@@ -11,48 +11,48 @@ replicas ips to the the /etc/hosts for example:
 """
 
 
-# class MongoDB(object):
-#     def __init__(self, mongo_user="", mongo_password="", mongo_host_port_array=""
-#                 , mongo_db="mapi", mongo_auth_db='admin', rs='rs0'):
-#         uri = f"mongodb://{quote_plus(mongo_user)}:{quote_plus(mongo_password)}@" \
-#               f"{mongo_host_port_array}/{mongo_db}?authSource={mongo_auth_db}"
-#         self.client = MongoClient(uri, w=1, journal=True, replicaSet=rs)
-#
-#     def find(self, db, collection):
-#         res = db[collection].find({})
-#         return res
-#
-#     def get_list_sites(self):
-#         sites = self.find('db_name', "sites")
-#         return sites
-#
-#     def _get_list_sites(self, db):
-#         self.site_list = []
-#         res = db.sites.find({})
-#         for site in res:
-#             self.site_list.append(site)
-#         return self.site_list
-#
-#     def count_subjects(self, db):
-#         return db.subjects.count()
-#
-#     def site_sync_status(self):
-#         db = self._get_db("mapi")
-#         site_list = self._get_list_sites(db)
-#         for site in site_list:
-#             return site['syncStatus']
-#
-#     def _get_db(self, db):
-#         return self.client[db]
-#
-#     def get_sites_id(self):
-#         db = self._get_db("mapi")
-#         site_ids = []
-#         site_list = self._get_list_sites(db)
-#         for site in site_list:
-#             site_ids.append(site['_id'])
-#         return site_ids
+class MongoDB(object):
+    def __init__(self, mongo_user="", mongo_password="", mongo_host_port_array=""
+                , mongo_db="mapi", mongo_auth_db='admin', rs='rs0'):
+        uri = f"mongodb://{quote_plus(mongo_user)}:{quote_plus(mongo_password)}@" \
+              f"{mongo_host_port_array}/{mongo_db}?authSource={mongo_auth_db}"
+        self.client = MongoClient(uri, w=1, journal=True, replicaSet=rs)
 
+    def find(self, db, collection):
+        res = db[collection].find({})
+        return res
 
-def test_basic(base_config):
-    base_config.hosts.host.MongoDB.connect()
+    def get_list_sites(self):
+        sites = self.find('db_name', "sites")
+        return sites
+
+    def _get_list_sites(self, db):
+        self.site_list = []
+        res = db.sites.find({})
+        for site in res:
+            self.site_list.append(site)
+        return self.site_list
+
+    def count_subjects(self, db):
+        return db.subjects.count()
+
+    def site_sync_status(self):
+        db = self._get_db("mapi")
+        site_list = self._get_list_sites(db)
+        for site in site_list:
+            return site['syncStatus']
+
+    def _get_db(self, db):
+        return self.client[db]
+
+    def get_sites_id(self):
+        db = self._get_db("mapi")
+        site_ids = []
+        site_list = self._get_list_sites(db)
+        for site in site_list:
+            site_ids.append(site['_id'])
+        return site_ids
+
+#
+# def test_basic(base_config):
+#     base_config.hosts.host.MongoDB.connect()
