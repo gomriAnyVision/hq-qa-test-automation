f09b57d4f6c7a0f252aeefc1edab488112d41adb
Omri Golan <omrig@anyvision.com>
2020-02-20 11:13:20 +0200

    Refactored the hq_pod_healthy function so that the loop that runs it is not inside it but outside in the healthy_cluster function that it only runs once and returns the True if the condition is true and False otherwise. Also changed the call to the ssh function to use my wrapper just to shorten the function

--- ssh.py
+++ ssh.py
@@ -28,6 +28,7 @@ ssh_logger.addHandler(file_handler)
 ssh_logger.addHandler(handler)
 
 # TODO: Remove duplicated ssh connection code and ssh data parasing
+# TODO: Create Retry soluation for connecting via ssh
 
 def disconnect_site_from_hq(**config):
     ssh = paramiko.SSHClient()
@@ -129,9 +130,6 @@ def k8s_cluster_status(**config):
 
 
 def consul_elected_leader(logger, **kwargs):
-    kwargs['timeout'] = None
-    timeout=kwargs['timeout'] if kwargs['timeout'] else 60
-    stop_time = time.time() + timeout
     command = """kubectl exec -ti $(kubectl get pod -l=app=hq | grep -v Terminating | grep -i 2/2 | awk {'print $1'}
 ) -c api-master -- curl http://consul-server-client:8500/v1/status/leader"""
     ssh = paramiko.SSHClient()
@@ -141,16 +139,10 @@ def consul_elected_leader(logger, **kwargs):
                 username=ssh_config['username'],
                 password=ssh_config['password'],
                 key_filename=None if ssh_config['pem_path'] == "" else kwargs['pem_path'])
-    while not time.time() > stop_time:
-        stdin, stdout, stderr = ssh.exec_command(command)
-        result = stdout.read().decode('utf-8')
-        if not result:
-            wait_for(10, "Sleeping while consul isn't healthy", logger)
-        else:
-            return True
-    else:
-        logger.error(f"Consul was unhealthy after {timeout} seconds")
-        return False
+    stdin, stdout, stderr = ssh.exec_command(command)
+    result = stdout.read().decode('utf-8')
+    print(result)
+    return True if result else False
 
 
 def _ssh_connect(hostname):
@@ -165,26 +157,15 @@ def _ssh_connect(hostname):
     return ssh
 
 
-def hq_pod_healthy(logger, **kwargs):
-    kwargs['timeout'] = None
-    timeout = kwargs['timeout'] if kwargs['timeout'] else 60
-    timeout = time.time() + timeout
+def hq_pod_healthy(logger, ip):
     command = "kubectl get pod -l=app=hq | grep -v Terminating | grep -i 2/2 | awk {\'print $1\'}"
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_config = config['vm'][0]['ssh']
-    ssh.connect(hostname=kwargs['ip'],
-                username=ssh_config['username'],
-                password=ssh_config['password'],
-                key_filename=None if ssh_config['pem_path'] == "" else kwargs['pem_path'])
-    while not time.time() > timeout:
-        stdin, stdout, stderr = ssh.exec_command(command)
-        result = stdout.read().decode("utf-8")
-        if result:
-            return True
-        else:
-            wait_for(10, "Sleeping while hq isn't healthy", logger)
-    return False
+    ssh = _ssh_connect(hostname=ip)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    result = stdout.read().decode("utf-8")
+    return True if result else False
 
 
 def mongo_has_primary(logger, ip, timeout=60):
@@ -213,7 +194,6 @@ def consul_nodes(logger, ip):
         decoded_res = result.decode('utf-8')
         json_res = json.loads(decoded_res)
         number_of_peers = len(json_res)
-        # TODO: change log level to debug
         logger.debug(f"Result: {decoded_res}")
         logger.debug(f"Number of peers: {number_of_peers}")
         return number_of_peers
