4d9050d34272176737e4b772d8a9d38229de63da
Omri Golan <omrig@anyvision.com>
2020-02-23 10:38:16 +0200

    Contiuned writing test

--- test_recognition_down_time.py
+++ test_recognition_down_time.py
@@ -2,7 +2,8 @@ import logging
 import sys
 import time
 
-from vm_management import MachineManagement, VmMgmt, stop_machine
+from socketio_client import verify_recognition_event
+from vm_management import MachineManagement, VmMgmt, stop_machine, healthy_cluster
 from test_end_to_end import HQ_MACHINES, get_sync_status
 
 """
@@ -17,31 +18,46 @@ Test flow
 
 """
 
-recog_down_time_logger = logging.getLogger(__name__)
-recog_down_time_logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler = logging.FileHandler("execution.log")
 handler = logging.StreamHandler(sys.stdout)
 file_handler.setFormatter(formatter)
-recog_down_time_logger.addHandler(file_handler)
-recog_down_time_logger.addHandler(handler)
+logger.addHandler(file_handler)
+logger.addHandler(handler)
 
 if __name__ == '__main__':
     machine_mgmt = MachineManagement(VmMgmt())
-    machine_mgmt.ensure_all_machines_started(recog_down_time_logger)
-    play_stream()
-    recog_down_time_logger.info("----------------------------------------------------------------")
-    recog_down_time_logger.info("                   STARTING MAIN TEST LOOP                      ")
-    recog_down_time_logger.info("----------------------------------------------------------------")
+    machine_mgmt.ensure_all_machines_started(logger)
+    # TODO: add function to play stream
+    logger.info("----------------------------------------------------------------")
+    logger.info("                   STARTING MAIN TEST LOOP                      ")
+    logger.info("----------------------------------------------------------------")
     wait_for_cluster = 30
-    # TODO: Add site befor test starts
+    # TODO: Add site before test starts
     while True:
         for machine, ip in HQ_MACHINES.items():
-            stop_machine(machine, wait_for_cluster, recog_down_time_logger)
+            healthy_cluster("Healthy", logger)
+            stop_machine(machine, wait_for_cluster, logger)
+            # healthy_cluster("Healthy", logger)
             HQ_MACHINES[machine] = None
-            sync_status = ""
-            while not sync_status == "synced":
-                time.sleep(10)
-                sync_status = get_sync_status()
-                recog_down_time_logger.debug(f"Sync status was: {sync_status} sleeping 10 seconds and trying again")
-                # TODO: Ensure we have subjects in HQ watchlist
+            # sync_status = ""
+            # while not sync_status == "synced":
+            #     time.sleep(10)
+            #     sync_status = get_sync_status()
+            #     logger.debug(f"Sync status was: {sync_status} sleeping 10 seconds and trying again")
+            # TODO: Ensure we have subjects in HQ watchlist
+            start_time = time.time()
+            while True:
+                logger.info("Started waiting for recognition's")
+                if verify_recognition_event(logger, 60):
+                    logger.error("Didn't get recognition's trying again")
+            end_time = time.time()
+            time_delta = start_time - end_time
+            logger.info(f"It took {time_delta} seconds to get recognition event")
+            result_times.append(time_delta)
+            start_machine(machine, wait_for_cluster, logger)
+            healthy_cluster("Healthy", logger)
+
+
