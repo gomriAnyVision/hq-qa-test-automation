8ba5f5d14bf23c40c11f1dae905e0f4978697d3b
Omri Golan <omrig@anyvision.com>
2020-02-26 10:22:59 +0200

    Added global vars for iteration number and average down time

--- test_recognition_down_time.py
+++ test_recognition_down_time.py
@@ -12,7 +12,8 @@ HQ_MACHINES = get_default_config()['hq_machines']
 SYNC_STATUS = None
 RESULT_TIMES = []
 WAIT_FOR_CLUSTER = 0
-ALL_RESULTS_TIMES = None
+ITERATION_NUMBER = 1
+AVERAGE_TIME = 0
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -41,22 +42,19 @@ if __name__ == '__main__':
     logger.info(f"Received config: {pformat(env_config)}")
     machine_mgmt = MachineManagement(VmMgmt())
     machine_mgmt.ensure_all_machines_started(logger)
+    healthy_cluster("Healthy", logger, alive_hq_node_ip(), minimum_nodes_running=3)
     # TODO: add function to play stream
+    # TODO: Add site before test starts
+    # TODO: Ensure we have subjects in HQ watchlist
     logger.info("----------------------------------------------------------------")
     logger.info("                   STARTING MAIN TEST LOOP                      ")
     logger.info("----------------------------------------------------------------")
-    # TODO: Add site before test starts
-    # hq_session = HQ()
-    # hq_session.login()
-    # site_id = hq_session.add_site(env_config['vm'][0])
     while True:
         for machine, ip in HQ_MACHINES.items():
             stop_node = time.time()
             logger.info(f"Stopping Machine:{machine} IP:{ip} ")
             stop_machine(machine, WAIT_FOR_CLUSTER, logger)
-            # healthy_cluster("Healthy", logger)
             HQ_MACHINES[machine] = None
-            # TODO: Ensure we have subjects in HQ watchlist
             task_wait_for_recog()
             received_recog = time.time()
             time_delta = received_recog - stop_node
@@ -64,9 +62,11 @@ if __name__ == '__main__':
             RESULT_TIMES.append(time_delta)
             start_machine(machine, WAIT_FOR_CLUSTER, logger)
             healthy_cluster("Healthy", logger, alive_hq_node_ip(), minimum_nodes_running=3)
-            average_time = calculate_average(RESULT_TIMES)
+            AVERAGE_TIME = calculate_average(RESULT_TIMES)
             HQ_MACHINES[machine] = ip
             logger.info(f"average time from stopping node to receviing recognition from already connected site: "
-                        f"{average_time}")
+                        f"{AVERAGE_TIME}")
+            ITERATION_NUMBER += 1
+            logger.info(f"Number of iterations: {ITERATION_NUMBER}")
 
 
