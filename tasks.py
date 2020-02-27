57f61c5a3c583a2aa85e0468fa699f40df6e3810
Omri Golan <omrig@anyvision.com>
2020-02-27 20:39:08 +0200

    Added task that attempts to add a site it successeds

--- tasks.py
+++ tasks.py
@@ -24,10 +24,38 @@ def wait_for_recog():
             tasks_logger.error("Failed to get event from HQ")
 
 
+def wait_for_add_site(hq_session, config):
+    while True:
+        if not hq_session.login():
+            continue
+        username, pem_path, password, site_extarnel_ip = get_ssh_params(config)
+        feature_toggle_master = consul_get_one("api-env/FEATURE_TOGGLE_MASTER", site_extarnel_ip)
+        tasks_logger.info(f"Connect to consul at {site_extarnel_ip} "
+                          f"and get FEATURE_TOGGLE_MASTER value = {feature_toggle_master}")
+        if feature_toggle_master == "false":
+            consul_set("api-env/FEATURE_TOGGLE_MASTER", "true", site_extarnel_ip)
+            delete_pod(ip=site_extarnel_ip,
+                       username=username,
+                       password=password,
+                       pem_path=pem_path,
+                       pod_name="site")
+            wait_for(20, "Waiting for api to restart after feature toggle master", tasks_logger)
+        if is_service_available(site_extarnel_ip, 3000) and is_service_available(site_extarnel_ip, 16180):
+            try:
+                tasks_logger.info(f"Changed FEATURE_TOGGLE_MASTER = 'true'")
+                site_id = hq_session.add_site(site_extarnel_ip)
+                tasks_logger.info(f"successfully added site with internal IP {site_extarnel_ip} "
+                                  f"and external IP {site_extarnel_ip}")
+                return site_id
+            except:
+                tasks_logger.error(f"Failed to add site with with external IP {site_extarnel_ip} "
+                                   f"Attempting to run the automation again")
+
+
 def delete_site(active_hq_node, hq_session, config):
+    hq_session.login()
     sites_id = hq_session.get_sites_id()
-    username, pem_path, password = config['vm'][0]['ssh'].values()
-    site_extarnel_ip = config['vm'][0]['site_extarnel_ip']
+    username, pem_path, password, site_extarnel_ip = get_ssh_params(config)
     try:
         remove_site_from_hq = hq_session.remove_site(sites_id)
         tasks_logger.info(f"Delete site from HQ results: {remove_site_from_hq}")
